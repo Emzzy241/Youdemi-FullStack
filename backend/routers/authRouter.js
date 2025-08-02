242d10f408f6384e3004cc56b745d9871f4ee63a
Emzzy241 <emzzyoluwole@gmail.com>
2025-08-02 20:47:10 +0100

    update auth router to ES Module syntax

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -1,16 +1,18 @@
 import express from "express"
-import authController from "./../controllers/authController"
+// import authController from "../controllers/authController.js"
+import authController from "./../controllers/authController.js"
 
+console.log("Reading the auth router file")
 const router = express()
 
 router.post("/signup", authController.signUp)
 router.post("/signin", authController.signIn)
 router.post("/signout", authController.signOut)
 
-router.patch("/send-verification-code", identifier, authController.sendVerificationCode)
-router.patch("/verify-verification-code", identifier, authController.verifyVerificationCode)
-router.patch("/change-password", identifier, authController.changePassword)
-router.patch("/send-forgot-password-code", identifier, authController.sendForgotPasswordCode)
-router.patch("verify-forgot-password-code", identifier, authController.verifyForgotPasswordCode)
+// router.patch("/send-verification-code", identifier, authController.sendVerificationCode)
+// router.patch("/verify-verification-code", identifier, authController.verifyVerificationCode)
+// router.patch("/change-password", identifier, authController.changePassword)
+// router.patch("/send-forgot-password-code", identifier, authController.sendForgotPasswordCode)
+// router.patch("verify-forgot-password-code", identifier, authController.verifyForgotPasswordCode)
 
 export default router
