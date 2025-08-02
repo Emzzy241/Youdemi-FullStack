cffe1306add6fd2b717c4971a622467afd1b4cff
Emzzy241 <emzzyoluwole@gmail.com>
2025-08-02 18:43:34 +0100

    add validation for User Sign Up

---
+++ backend/middlewares/validator.js
@@ -0,0 +1,14 @@
+import Joi from "joi";
+
+exports.signUpSchema = Joi.object({
+    email: Joi.string()
+        .min(6)
+        .max(60)
+        .required()
+        .email({
+            tlds: { allow: ["com", "net"] }
+        }),
+    password: Joi.string()
+        .required()
+        .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$'))
+})
