d32e786b4c47a082e1c7c5de7a5961dc0cd2e37d
Emzzy241 <emzzyoluwole@gmail.com>
2025-08-02 18:42:04 +0100

    add Authentication for Signing Up a User

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,38 @@
 import authRoutes from "./../routers/authRouter"
+import { signUpSchema } from "./../middlewares/validator"
+import User from "../models/user"
 
 const signUp = async (req, res) => {
-    
+    const { email, password } = req.body
+
+    try {
+        const { error, value } = signUpSchema.validate({ email, password })
+        if (error) {
+            return res.status(401).json({ success: false, message: error.details[0].message})
+        }
+
+        const existingUser = await User.findOne({ email })
+
+        if (existingUser) { 
+            res.status(401).json({ success: false, message: "User already exists, sign in instead" })
+        }
+
+        const hashedPassword = await doHash(password, 12)
+        const newUser = new User({
+            email,
+            password
+        })
+        const result = await newUser.save();
+        result.password = undefined;
+        res.status(201).json({
+            success: true,
+            message: "Your account has been created successfully",
+            result
+        })
+
+    } catch (error) {
+        console.log(error.message)
+    }
 }
 
 const signIn = async (req, res) => {
