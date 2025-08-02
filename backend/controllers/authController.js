ed31dd5e5a87677ee666b1ea309aa1e5b70b4deb
Emzzy241 <emzzyoluwole@gmail.com>
2025-08-02 16:25:14 +0100

    add auth controller for user authentication

---
+++ backend/controllers/authController.js
@@ -0,0 +1,44 @@
+import authRoutes from "./../routers/authRouter"
+
+const signUp = async (req, res) => {
+    
+}
+
+const signIn = async (req, res) => {
+
+}
+
+const signOut = async (req, res) => {
+
+}
+
+const sendVerificationCode = async (req, res) => {
+
+}
+
+const verifyVerificationCode = async (req, res) => {
+
+}
+
+const changePassword = async (req, res) => {
+
+}
+
+const sendForgotPasswordCode = async (req, res) => {
+
+}
+
+const verifyForgotPasswordCode = async (req, res) => {
+
+}
+
+export default {
+    signUp,
+    signIn,
+    signOut,
+    sendVerificationCode,
+    verifyVerificationCode,
+    changePassword,
+    sendForgotPasswordCode,
+    verifyForgotPasswordCode
+}
