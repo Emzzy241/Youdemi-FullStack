cd0c34d14bf1a4150cf261cc0d257c5ddb6245c5
Emzzy241 <emzzyoluwole@gmail.com>
2025-08-03 08:05:55 +0100

    add authentication for User sign in

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 // import authRoutes from "./../routers/authRouter"
-import { signUpSchema } from "../middlewares/validator.js"
+import jwt from "jsonwebtoken"
+import { signInSchema, signUpSchema } from "../middlewares/validator.js"
 import { doHash, doHashValidation, hmacProcess } from "../utils/hashing.js"
 import User from "../models/user.js"
 
@@ -9,12 +10,12 @@ const signUp = async (req, res) => {
     try {
         const { error, value } = signUpSchema.validate({ email, password })
         if (error) {
-            return res.status(401).json({ success: false, message: error.details[0].message})
+            return res.status(401).json({ success: false, message: error.details[0].message })
         }
 
         const existingUser = await User.findOne({ email })
 
-        if (existingUser) { 
+        if (existingUser) {
             res.status(401).json({ success: false, message: "User already exists, sign in instead" })
         }
 
@@ -37,7 +38,48 @@ const signUp = async (req, res) => {
 }
 
 const signIn = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const { error, value } = signInSchema.validate({ email, password })
+        if (error) {
+            return res.status(401).json({ success: false, message: error.details[0].message })
+        }
 
+        const existingUser = await User.findOne({ email }).select("+password")
+
+        if (!existingUser) {
+            return res.status(401).json({ success: false, message: "User does not exist, sign up for an account today" })
+        }
+
+        const result = await doHashValidation(password, existingUser.password)
+
+        if (!result) {
+            return res.status(401).json({ success: false, message: "Invalid credentials" })
+        }
+
+        const token = jwt.sign({
+            userId: existingUser._id,
+            email: existingUser.email,
+            verified: existingUser.verified
+        },
+            process.env.TOKEN_SECRET,
+            {
+                expiresIn: "8h"
+            }
+        );
+
+        res.cookie("Authorization", "Bearer", + token, {
+            expires: new Date(Date.now() + 8
+                * 3600000), httpOnly: process.env.NODE_ENV === 'production', secure: process.env.NODE_ENV
+        }).json({
+            success: true,
+            token,
+            message: "logged in successfully"
+        });
+
+    } catch (error) {
+        console.log(error.message)
+    }
 }
 
 const signOut = async (req, res) => {
