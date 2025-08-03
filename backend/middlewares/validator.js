da3817f218b4ef6aa53d4ba052862ea47264cf3d
Emzzy241 <emzzyoluwole@gmail.com>
2025-08-03 08:06:32 +0100

    add validation for User sign In

--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -14,5 +14,14 @@ export const signUpSchema = Joi.object({
 })
 
 export const signInSchema = Joi.object({
-
+    email: Joi.string()
+        .min(6)
+        .max(60)
+        .required()
+        .email({
+            tlds: { allow: ["com", "net"] }
+        }),
+    password: Joi.string()
+        .required()
+        .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$'))
 })
