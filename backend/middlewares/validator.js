004658f56a2bafb66d3cb8608ff349d6dc9a7f5f
Emzzy241 <emzzyoluwole@gmail.com>
2025-08-11 18:11:09 +0100

    update acceptCodeSchema() validator

--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -25,3 +25,14 @@ export const signInSchema = Joi.object({
         .required()
         .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$'))
 })
+
+export const acceptCodeSchema = Joi.object({
+    email: Joi.string()
+        .min(6)
+        .max(60)
+        .required()
+        .email({
+            tlds: { allow: ["com", "net"] }
+        }),
+    providedCode: Joi.number().required()
+})
