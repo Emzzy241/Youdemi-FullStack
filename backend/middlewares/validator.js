2176b2d2468d2f642e7c70b5aa42675820547495
Emzzy241 <emzzyoluwole@gmail.com>
2025-08-12 15:46:33 +0100

    add forgotPasswordCodeSchema validator

--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -46,3 +46,14 @@ export const changePasswordSchema = Joi.object({
         .required()
         .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$'))
 })
+
+export const forgotPasswordCodeSchema = Joi.object({
+    email: Joi.string()
+        .min(6)
+        .max(60)
+        .required()
+        .email({
+            tlds: { allow: ["com", "net"] }
+        }),
+    providedCode: Joi.number().required()
+})
