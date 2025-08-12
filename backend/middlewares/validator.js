0669802bde1e930c666322147597a1a912bbc343
Emzzy241 <emzzyoluwole@gmail.com>
2025-08-12 16:33:50 +0100

    add create course schema

--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -57,3 +57,29 @@ export const forgotPasswordCodeSchema = Joi.object({
         }),
     providedCode: Joi.number().required()
 })
+
+export const acceptForgotPasswordCodeSchema = Joi.object({
+    email: Joi.string()
+        .min(6)
+        .max(60)
+        .required()
+        .email({
+            tlds: { allow: ["com,", "net"]}
+        }),
+    providedCode: Joi.number().required(),
+    newPassword: Joi.string()
+        .required()
+        .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$'))
+})
+
+export const CreateCourseSchema = Joi.object({
+    title: Joi.string()
+        .min(3)
+        .max(60)
+        .required(),
+    description: Joi.string()
+        .min(6)
+        .max(60)
+        .required(),
+    userId: Joi.string().required()
+})
