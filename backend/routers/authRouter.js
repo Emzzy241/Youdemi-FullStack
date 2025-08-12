a5700a71b8248fcdaa9ce09b83904d0cbbd005a1
Emzzy241 <emzzyoluwole@gmail.com>
2025-08-12 15:47:20 +0100

    add sendForgotPasswordCode route

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -12,7 +12,7 @@ router.post("/signout", authController.signOut)
 router.patch("/send-verification-code", identifier, authController.sendVerificationCode)
 router.patch("/verify-verification-code", identifier, authController.verifyVerificationCode)
 router.patch("/change-password", identifier, authController.changePassword)
-// router.patch("/send-forgot-password-code", identifier, authController.sendForgotPasswordCode)
+router.patch("/send-forgot-password-code", identifier, authController.sendForgotPasswordCode)
 // router.patch("verify-forgot-password-code", identifier, authController.verifyForgotPasswordCode)
 
 export default router
