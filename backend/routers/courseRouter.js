8329b431ac0f15711914bcb9e1585e7a633498ab
Emzzy241 <emzzyoluwole@gmail.com>
2025-08-25 17:16:42 +0100

    update create course endpoint for authorization

--- backend/routers/courseRouter.js
+++ backend/routers/courseRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import courseController from "../controllers/courseController.js"
+import { identifier } from "../middlewares/identification.js";
 
 const router = express.Router()
 
@@ -9,7 +10,7 @@ router.get("/", async (req, res) => {
 
 router.get("/courses", courseController.getAllCourses)
 
-router.post("/courses/new", courseController.CreateCourse)
+router.post("/courses/new", identifier, courseController.createCourse)
 
 router.get("/courses/:id", courseController.getCourse)
 
