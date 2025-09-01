4cd759de2d540c66ad5ba2c84dbf3dea9d146f7f
Emzzy241 <emzzyoluwole@gmail.com>
2025-09-01 19:43:14 +0100

    add other frontend integration code for courses

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -3,3 +3,30 @@ async function heyThere() {
     console.log(res)
     alert("Hey there")
 }
+
+async function loginUser(email, password) {
+    const res = await fetch("https://youdemi-fullstack.onrender.com/api/signin", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ username: email, password })
+    })
+
+    const data = await res.json()   
+}
+
+async function registerUser(email, password) {
+    const res = await fetch("https://youdemi-fullstack.onrender.com/api/signup", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ username: email, password})
+    })
+}
+
+async function allCourses() {
+    const res = await fetch("https://youdemi-fullstack.onrender.com/api/v1/course/courses", {
+        method: "GET"
+    })
+
+    console.log(res)
+    
+}
