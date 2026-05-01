561ab0326beef435a25e13efd4a38b0aeb2e67b3
Emzzy241 <emzzyoluwole@gmail.com>
2026-05-01 17:55:28 +0100

    fixing catalog courses bugs

--- backend/routers/publicRouter.js
+++ backend/routers/publicRouter.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authController from "./../controllers/authController.js"
+import catalogController from "../controllers/catalogController.js"
 import courseController from "../controllers/courseController.js"
 
 console.log("Reading the publicRouter file")
@@ -9,14 +10,17 @@ const publicRouter = express()
 
 // Public routes for  authentication
 publicRouter.get("/", authController.authGreeting)
-publicRouter.get("/courses/catalog", courseController.getAllCatalogCourses)
+publicRouter.get("/course", courseController.courseGreeting)
+publicRouter.get("/courses/catalog/greeting", catalogController.catalogGreeting)
+publicRouter.get("/courses/catalog", catalogController.getAllCatalogs)
+publicRouter.post("/courses/catalog/new", catalogController.createCatalog)
+publicRouter.get("/courses/catalog/:id", catalogController.getCatalog)
+publicRouter.patch("/courses/catalog/:id", catalogController.updateCatalog)
+publicRouter.delete("/courses/catalog/:id", catalogController.deleteCatalog)
 publicRouter.post("/signup", authController.signUp)
 publicRouter.post("/signin", authController.signIn)
 publicRouter.post("/signout", authController.signOut)
 publicRouter.patch("/send-forgot-password-code", authController.sendForgotPasswordCode)
 publicRouter.patch("/verify-forgot-password-code", authController.verifyForgotPasswordCode)
-
-// Public routes for  course
-publicRouter.get("/course", courseController.courseGreeting)
 
 export default publicRouter
