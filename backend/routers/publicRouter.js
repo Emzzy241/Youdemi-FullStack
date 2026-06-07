8b4b21ee84d9c878f9b0d73a2662d34237f3697d
Emzzy241 <emzzyoluwole@gmail.com>
2026-06-07 02:19:03 +0100

    update public routers + add Test routes for RBAC

--- backend/routers/publicRouter.js
+++ backend/routers/publicRouter.js
@@ -2,8 +2,9 @@ import express from "express"
 import authController from "./../controllers/authController.js"
 import catalogController from "../controllers/catalogController.js"
 import courseController from "../controllers/courseController.js"
-
-console.log("Reading the publicRouter file")
+import protect from "../middlewares/protect.js"
+import authorizeRoles from "../middlewares/authorizeRoles.js"
+// console.log("Reading the publicRouter file")
 
 
 const publicRouter = express()
@@ -15,6 +16,23 @@ publicRouter.get("/catalog/greeting", catalogController.catalogGreeting)
 publicRouter.get("/catalog", catalogController.getAllCatalogs)
 publicRouter.post("/catalog/new", catalogController.createCatalog)
 publicRouter.get("/catalog/:id", catalogController.getCatalog)
+publicRouter.get(
+    "/me",
+    protect,
+    (req, res) => {
+        res.json(req.user);
+    }
+);
+
+publicRouter.get(
+   "/admin-test",
+   protect,
+   authorizeRoles("admin"),
+   (req,res)=>{
+      res.send("success");
+   }
+);
+
 publicRouter.patch("/catalog/:id", catalogController.updateCatalog)
 publicRouter.delete("/catalog/:id", catalogController.deleteCatalog)
 publicRouter.post("/signup", authController.signUp)
